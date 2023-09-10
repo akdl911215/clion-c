3c855c118a9958100929ac72e90adbf3804b8db1
akdl911215 <akdl911215@naver.com>
2023-09-11 00:06:41 +0900

    oprt basic - std

--- OprtBasic10-2/main.c
+++ OprtBasic10-2/main.c
@@ -2,9 +2,9 @@
 
 int main() {
 
-    int nData = 123;
+    int nData = -123;
+    // 16진수 -7B
 
-    // 123인 경우
     printf("%X\n", nData);
     // 128 64 32 16 8 4 2 1
     //  0   1  1  1 1 0 1 1
